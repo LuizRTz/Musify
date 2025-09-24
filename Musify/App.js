6a411e6afe2c222491fb5335c0b958d8759f1708
LuizRTz <luizotaviosifrade10@gmail.com>
2025-09-24 13:55:58 -0300

    Player Funcionando

--- Musify/App.js
+++ Musify/App.js
@@ -3,7 +3,9 @@ import { StatusBar } from 'expo-status-bar';
 import { Dimensions, Image, FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Animated } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Slider from '@react-native-community/slider';
+import { Audio } from 'expo-av';
 import songs from './model/data';
+
 
 const { width, height } = Dimensions.get('window');
 
@@ -26,7 +28,7 @@ export default function App() {
     });
   }, []);
 
-  const renderSongs = ([ item, index ]) => {
+  const renderSongs = ({ item, index }) => {
     return (
       <View style={styles.mainImageWrapper}>
         <View style={[styles.imageWrapper, styles.elevation]}>
@@ -36,11 +38,67 @@ export default function App() {
     )
   };
 
+  const loadSound = async () => {
+    const { sound } = await Audio.Sound.createAsync(songs[songIndex].url);
+    setSound(sound);
+    const status = await sound.getStatusAsync();
+    status.isLooping = isLooping;
+    await sound.setIsLoopingAsync(isLooping);
+    setSongStatus(status);
+    setIsPlaying(false);
+  }
+
+  useEffect(() => {
+    if (sound) {
+      sound.unloadAsync();
+    }
+    loadSound();
+    return () => {
+      if (sound) {
+        sound.unloadAsync();
+      }
+    }
+  }, [songIndex]);
+
+  const play = async () => {
+    if (sound) {
+      setIsPlaying(true);
+      await sound.playAsync();
+    }
+  }
+
+  const pause = async () => {
+    if (sound) {
+      setIsPlaying(false);
+      await sound.playAsync();
+    }
+  }
+
+  const handlePlayPause = async () => {
+    if (isPlaying) {
+      await pause();
+    } else {
+      await play();
+    }
+  }
+
+  const stop = async () => {
+    if (sound) {
+      await sound.stopAsync();
+      sound.unloadAsync();
+      await loadSound();
+    }
+  }
+
+  const skipToPrevious = () => {
+    
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.main}>
 
-      <Animeated.FlatList
+      <Animated.FlatList
         data={songs}
         renderItem={renderSongs}
         keyExtractor={item => item.id}
@@ -90,7 +148,7 @@ export default function App() {
         <TouchableOpacity>
           <Ionicons name='play-skip-back-outline' size={35} color='#FFD369'/>
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handlePlayPause}>
           <Ionicons name={isPlaying ? 'pause-circle' : 'play-circle'} size={75} color='#FFD369'/>
         </TouchableOpacity>
         <TouchableOpacity>
